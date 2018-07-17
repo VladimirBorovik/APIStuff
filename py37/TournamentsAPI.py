a3013ef613b135d4fb74cdd298cf89ea712add3c
VladimirBorovik <vladimir.borovyk89@gmail.com>
2018-07-17 16:07:52 +0300

    VCS set up
    VCS set up

--- py37/TournamentsAPI.py
+++ py37/TournamentsAPI.py
@@ -3,10 +3,14 @@ import requests
 import telepot
 
 
+
 bot = telepot.Bot('661730605:AAGMhc2ML5lxF5mxNCWIRqlT6g7Yk5z4p-0')
 meta_info = bot.getMe()
 
+
 print(meta_info)
+
+#print(meta_info)
 
 #response = requests.get("https://api.sportradar.us/soccer-t3/eu/en/teams/sr:competitor:1/profile.json?api_key=rpjetjuheffkq2attc4g9hq8")
 #json_data = json.loads(response.content)
